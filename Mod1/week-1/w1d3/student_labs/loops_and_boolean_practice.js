c8307dc555ddc66dd300aba0f72f68c5c1019494
Sam Sander <samsander@sams-mbp.home>
2020-06-03 22:11:52 -0400

    practice

---
+++ Mod1/week-1/w1d3/student_labs/loops_and_boolean_practice.js
@@ -0,0 +1,11 @@
+for (let hi = 1; hi < 16; hi++) {
+    console.log('hi');
+}
+
+for (let i = 0; i < 30; i++); {
+    console.log ('YEP');
+}
+
+console.log(Math.random());
+
+const fave_day = sunday
