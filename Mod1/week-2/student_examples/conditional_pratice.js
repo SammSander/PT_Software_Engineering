5289dd97e4322c65fe388c5f478db7c27eb3992a
Sam Sander <samsander@sams-mbp.home>
2020-06-03 21:35:30 -0400

    examples

---
+++ Mod1/week-2/student_examples/conditional_pratice.js
@@ -0,0 +1,37 @@
+/*
+const isPartyTonight = false;
+const miniGolfCoupon = false;
+const saleAtTheMall = false;
+if (isPartyTonight) {
+    console.log('I am going to party like its 1999') }
+    else if (miniGolfCoupon) {
+        console.log('Hole in one!')
+    }
+    else if (saleAtTheMall) {
+        console.log('Buy more')
+    }
+    else {
+        console.log('Cookies')
+    }
+    */
+
+
+
+    // the const is blue
+    //if the coloer is green is will log the ew statment
+    //if the const is blue it will log the yum statment
+ /*
+    let mmColor = 'blue'
+    if (mmColor = green) {
+        console.log (EW!)
+    }
+    else {
+        console.log(YUM!)
+    } */
+
+
+for (let i = 0; i<= 10; i++) {
+    if (i===1) {
+        console.log(i)
+    }
+}
