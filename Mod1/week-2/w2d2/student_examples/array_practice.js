b4dc19e10d29c3dbe341ee864f8c594a654d1c5b
Sam Sander <samsander@sams-mbp.home>
2020-06-04 20:51:09 -0400

    examples

---
+++ Mod1/week-2/w2d2/student_examples/array_practice.js
@@ -0,0 +1,52 @@
+/*
+console.log('test');
+
+const sayHello = "YO"
+console.log(typeof sayHello); // what data type - string
+
+const arr ['string', 1];
+console.log(typeof arr); // object 
+
+const num = 1;
+const bool = ture;
+console.log(typeof mun); // -num
+console.log(typeof bool);  // -boolean
+*/
+
+/*
+const faveFood = ['rice cakes', 'tacos', 'onions'];
+console.log(faveFood);
+console.log(faveFood[0]);
+
+const ghostBusters = ['Venkman', 'Spengler', 'Stantz', 'Zeddemore', 'Melnitz', 'Barrett', 'Tully']
+console.log(ghostBusters [0])
+console.log(ghostBusters [2]);
+console.log(ghostBusters.length);
+*/
+/*
+const numbers = [21, 18, 5, 3, 2, 1, 1];
+numbers[2] = null;
+numbers[0] *= 10;
+console.log(numbers);
+*/
+/*
+const drSuess = ["cat in the hat",'green eggs and ham', 'grinch', 'thing one', 'thing two', 
+'cindy loo who', 'jojo mcdoo'];
+    console.log(drSuess);
+    for (let i=1; < drSuess.length; i++) {
+    console.log(deSuess [i]);
+    }
+
+    for (let i = 0; i < drSuess.length; i++) {
+        if (i % 2 !== 0) {
+            console.log(frSuess[i]);
+        }
+  }
+*/
+
+
+//lab
+
+const favMovie = ['heathers', 'jenifers body', 'buffy'];
+favMovie.pop();
+  console.log(favMovie);
