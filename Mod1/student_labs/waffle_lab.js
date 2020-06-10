89a24b7f81320e6a216dd7a06d42b554114f24d2
Sam Sander <samsander@sams-mbp.home>
2020-06-09 20:59:31 -0400

    waffles

---
+++ Mod1/student_labs/waffle_lab.js
@@ -0,0 +1,14 @@
+/* psuedo code is more plain english
+a way do describe what you are meaning to code
+start off in plain englidh
+then get a little more indepth with emglish 
+until you can full understand
+what you want to code. */
+
+/* take the waffels from the freezer 
+put waffels in toster 
+toast 
+take out of toaster
+put butter on waffles 
+put syurp on waffles 
+eat waffles */
