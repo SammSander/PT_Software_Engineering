5cefbd907b339bd108812fbe7425747f11ce85df
Sam Sander <samsander@sams-mbp.home>
2020-06-15 20:59:00 -0400

    took it easy

--- Mod1/week-3 /w3d3/homework/answers.js
+++ Mod1/week-3 /w3d3/homework/answers.js
@@ -5,3 +5,17 @@
 //5: object and array
 //6: object and array
 
+// take it easy 
+//1
+const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'violet']
+//2
+console.log(rainbow[4]);
+//3
+const sam = {
+    food: "rice noodles",
+    hobby: "movies",
+    city: "Columbus",
+    datatype: "array"
+}
+//4
+console.log(sam.hobby);
