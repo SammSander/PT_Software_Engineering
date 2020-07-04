357aee24534b4471d74e10f4b1b0e814c0fb4aef
Sam Sander <samsander@sams-mbp.home>
2020-07-03 21:02:21 -0400

    Combine objects, arrays, and functions

---
+++ Mod1/week-4/w4d2/combining_datatypes.js
@@ -0,0 +1,31 @@
+//Combining Datatypes
+
+//1
+const crayonBox = {
+    colors: ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+}
+console.log(crayonBox.colors[0]);
+
+//2 
+const bottle = {
+    cap: {
+        material: 'metal',
+        colour: 'white'
+    }
+}
+console.log(bottle.cap.material);
+
+//3
+const receipt = [
+    {name: 'tofu', price: 3.50},
+    {name: 'chippies', price: 2.50},
+    {name: 'pepper', price: 1.50}
+]
+console.log(receipt[0].price);
+
+//4
+const apartmentBuilding = [
+    tenants = ['Karen', 'Jack Jackerson', 'MR.', 'Jarvis']
+]
+console.log(apartmentBuilding[0][0])
+
