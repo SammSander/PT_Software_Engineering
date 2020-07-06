3e302d83652c46ce245b2a0b0c5b51002abccc72
Sam Sander <samsander@sams-mbp.home>
2020-07-05 20:24:39 -0400

    dougie and pizza rat have fought

---
+++ Mod1/week-5/w5d1/homework/donut_adventure/adventure.js
@@ -0,0 +1,91 @@
+// our hero 
+
+class Hero {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.weapons = {sprinkleSpray: 5, sugarShock: 10};
+        this.catchPhrase = ['I\'m fresher than day old pizza', 'You can\'t count my calories'];
+    }
+    talkSass() {
+        let phrases = this.catchPhrase;
+        let random = Math.random();
+        let totalPhrase = this.catchPhrase.length;
+        let randomIndex = Math.floor(random * totalPhrase);
+        let randomPhrase = phrases[randomIndex];
+        return randomPhrase;
+    }
+    announceHealth() {
+        return `${this.name}'s health is ${this.health}.`;
+    }
+    announceHealth() {
+        return `${this.name}'s health is ${this.health}.`;
+    }
+        fight(enemy) {
+            //console.log('i\'m ready to rumble');
+            let random = Math.round(Math.random());
+            if (random == 1) {
+                enemy.health -= this.weapons.sugarShock;
+                return (`${this.name} has used sugar shock`)
+            } else {
+                enemy.health -= this.weapons.sprinkleSpray;
+                return (`${this.name} has used sprinkle spray`)
+            }
+        }
+}
+const Dougie = new Hero('Dougie');
+
+// enemy
+
+class Enemy {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.weapons = {pepperoniStars: 5, cheeseGrease: 10};
+        this.catchPhrase = ['I\'m youtube famous', 'I\'m more dangerous than an uncovered sewer'];
+    }
+    talkSmack() {
+        let phrases = this.catchPhrase;
+        let random = Math.random();
+        let totalPhrase = this.catchPhrase.length;
+        let randomIndex = Math.floor(random * totalPhrase);
+        let randomPhrase = phrases[randomIndex];
+        return randomPhrase;
+    }
+    announceHealth() {
+        return `${this.name}'s health is ${this.health}.`;
+    }
+    fight(hero) {
+        //return 'I\'m gonna flatten you like a slice of pepperoni'
+            let random = Math.round(Math.random());
+            if (random == 1) {
+                hero.health -= this.weapons.pepperoniStars;
+                return (`${this.name} has used pepperoni stars`)
+            } else {
+                hero.health -= this.weapons.cheeseGrease;
+                return (`${this.name} has used cheese grease`)
+            }
+        }
+}
+const PizzaRat = new Enemy('Pizza Rat');
+
+// walking odwn the street
+
+console.log(Dougie.talkSass());
+console.log(PizzaRat.talkSmack());
+console.log(Dougie.announceHealth());
+console.log(PizzaRat.announceHealth());
+
+// fight 
+
+
+
+console.log(Dougie.fight(PizzaRat));
+console.log(PizzaRat.fight(Dougie));
+console.log(PizzaRat.announceHealth());
+console.log(Dougie.announceHealth());
+
+
+
+
+
