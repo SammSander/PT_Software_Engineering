d84e19e8fdabe82625d5a79c60a185a0730cd602
Sam Sander <samsander@sams-mbp.home>
2020-08-25 20:52:08 -0400

    w2d3

---
+++ Mod1/week-2/w2d3/homework.js
@@ -0,0 +1,33 @@
+/*
+1. Why do you think Heinz made this change?
+- so the customers can use the product with more ease. 
+
+2. Discuss - How does this apply to web development?
+- Having a user friendly interface in web development is important because it determinds how they will interact with the product.
+a good UI could make the user be able or not able to use the app, and if they cant use it they will not come back. 
+
+3. What's a site that you use all the time that has good UX?
+YouTube
+How is that site:
+Goal Focused - The site allows you to search & watch videos based on the content that the user is looking for.
+Invisible - easy to navigate through.
+Forgiving - If you make a mistake you can always fix it easily 
+Consistent - The pages are all a similar in layout.
+Smooth Onboarding - When you go the site it's easy to figure out how to navigate through it without a lof of instructions.
+
+4. We're going to create the game tic-tac-toe ... but before we begin, what are some user stories that we'll want to make sure we cover?
+🐘 Reminder - Write from the perspective of the user: "As a user I want to be able to click on x and have y happen" or “As a ____, I need a ____ in order to ____”.
+- As a user I want to be able to select if I am "X" or "O".
+- As a user I want the "X" and "O" to be different colors.
+- As a user I want to be able to view the tic-tac-toe layout (#).
+- As a user I want to be able to see the cursor over the area that I am thinking about selecting.
+- As a user I want to be able to know if I can or can't place my letter over the area that I selected.
+- As a user I want to hear and see something on my screen when I select the area for the letter I placed. 
+- As a user I want to be able to hear and see my competitors choice once they have selected an area.
+- As a user I want to know wether I won, lost, or it is a tie at the end of the game.
+- As a user I want the winner to have a line going through the 3 in a row.
+- As a user I would like a prompt to restart the game once it's completed.
+
+
+
+*/
