9cb00696f8fbadf845a5e2c2aca4f26a61447a9a
Sam Sander <samsander@Sams-MBP.attlocal.net>
2020-09-02 11:52:00 -0400

    homework

---
+++ Mod-2/week9/w9d3/homework/jonathanpatterson/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ClientsComponent } from './clients/clients.component';
+import { SupComponent } from './sup/sup.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ClientsComponent,
+    SupComponent,
+    PortfolioComponent,
+    ContactComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
